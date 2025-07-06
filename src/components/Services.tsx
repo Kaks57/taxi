451b7f435a3a62f6a4c591e3476feefe5f63d8a2
Kaks57 <kmahtaj57@gmail.com>
2025-07-06 19:40:43 +0200

    azeazeazeaz

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -33,7 +33,7 @@ const services = [
 
 export const Services = () => {
   return (
-    <section id="services" className="py-16 bg-darkblue text-white">
+    <section id="services" className="py-16 bg-grey text-white">
       <div className="container">
         <div className="text-center mb-12">
           <h2 className="text-3xl font-bold mb-4 text-yellow-400">Nos Services</h2>
